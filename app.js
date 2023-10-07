7c05b980a3731a6a9dd1ee49558d3bcfc3331e70
TheJesterEnd <kakhniashvililuka@gmail.com>
2023-10-07 21:42:45 +0400

    add new Georgian Language

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 const birthYearInput = document.getElementById("num");
 const pElement = document.querySelector("p");
+const h1Element = document.querySelector("h1");
+const h2Element = document.querySelector("h2");
+const languageButton = document.getElementById("languageButton");
 
+let isGeorgian = false;
+
+function toggleLanguage() {
+    isGeorgian = !isGeorgian;
+
+    if (isGeorgian) {
+        h1Element.textContent = "მოდი გამოვიცნობ შენს ასაკს";
+        h2Element.textContent = "შეიყვანე შენი დაბადების წელი";
+        pElement.textContent = isGeorgian ? "ასაკი:" : "AGE:";
+        languageButton.textContent = "EN";
+    } else {
+        h1Element.textContent = "Let Me Guess Your AGE";
+        h2Element.textContent = "Enter Your Birth Year";
+        pElement.textContent = isGeorgian ? "ასაკი:" : "AGE:";
+        languageButton.textContent = "KA";
+    }
+}
 
 function myFunction() {
     const birthYear = parseInt(birthYearInput.value);
-    
-    if (!isNaN(birthYear) && birthYear >=1925 && birthYear <=2022) {
+
+    if (!isNaN(birthYear) && birthYear >= 1925 && birthYear <= 2022) {
         const currentYear = new Date().getFullYear();
         const age = currentYear - birthYear;
 
-        pElement.style.color ="aquamarine"
-        pElement.textContent = `AGE: ${age}`;
+        pElement.style.color = "aquamarine";
+        pElement.textContent = isGeorgian ? `ასაკი: ${age}` : `AGE: ${age}`;
+
+    } else if (birthYearInput.value === "") {
+        pElement.textContent = isGeorgian ? `ასაკი: ${age}` : `AGE: ${age}`;
     } else {
         pElement.style.color = "red";
-        pElement.textContent = "Please Enter a valid birth year.";
+        pElement.textContent = isGeorgian ? "გთხოვთ შეიყვანოთ სწორი დაბადების წელი." : "Please Enter a valid birth year.";
     }
 }
+
+document.getElementById("submit").addEventListener("click", myFunction);
+
+
+
